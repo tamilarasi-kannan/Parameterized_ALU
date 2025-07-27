6b49ce53f35464e6b74d1e025c224a69f1dc9a58
Tamilarasi Kannan <115526719+tamilarasi-kannan@users.noreply.github.com>
2025-07-27 22:38:39 +0530

    Create alu_gen.sv

---
+++ alu_gen.sv
@@ -0,0 +1,16 @@
+class alu_gen;
+  transaction tx;
+
+	function new();
+
+	endfunction
+
+	task run();
+		$display("Inside generator block");
+      repeat(count)begin
+      	tx=new();
+      	assert( tx.randomize());
+		gen2bfm.put(tx);
+      end
+	endtask
+endclass
